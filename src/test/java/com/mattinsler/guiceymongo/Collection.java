26ec0fd1e30db952be409abb280c232685c64bc4
mattinsler <matt.insler@gmail.com>
2011-02-12 03:33:28 -0500

    example collection for query DSL

---
+++ src/test/java/com/mattinsler/guiceymongo/Collection.java
@@ -0,0 +1,38 @@
+package com.mattinsler.guiceymongo;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mattinsler
+ * Date: 11/28/10
+ * Time: 3:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Collection {
+//    public static class FooEntity implements IsData {
+//        public static final StringProperty Name = new StringProperty("name");
+//        public static final class Address extends AbstractProperty<Address, Clause<Address>> {
+//            public Address() {
+//                super("address");
+//            }
+//            public static final StringProperty Street = new StringProperty("street", Address.class);
+//            public static final StringProperty City = new StringProperty("city", Address.class);
+//            public static final StringProperty State = new StringProperty("state", Address.class);
+//            public static final StringProperty ZipCode = new StringProperty("zip_code", Address.class);
+//        }
+//    }
+//
+//    public <T, C extends Clause<T>> C where(Property<T, C> property) {
+//        return null;
+//    }
+//
+//    @Test
+//    public void testQuery() {
+//        Collection collection = null;
+//
+//        FooEntity e = collection.where(FooEntity.Name).is("").select(FooEntity.class).orderBy(FooEntity.Name).ascending().executeOne();
+//
+//        collection.where(FooEntity.Name).is("Matt").and(FooEntity.Address.Street).isLike("^Over").select(null).execute();
+//
+//        collection.where(FooEntity.Address).exists();
+//    }
+}
